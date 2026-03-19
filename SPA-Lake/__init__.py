88e8fc7c37d78c84bd9586acb01fdfa04e16b85e
albertmena <12760268+albertmena@users.noreply.github.com>
2026-03-19 10:21:15 +0100

    second push

---
+++ SPA-Lake/__init__.py
@@ -0,0 +1,43 @@
+# **************************************************************************
+# *
+# * Authors: Alberto Garcia Mena   (alberto.garcia@cnb.csic.es)
+# *
+# * Unidad de  Bioinformatica of Centro Nacional de Biotecnologia , CSIC
+# *
+# * This program is free software; you can redistribute it and/or modify
+# * it under the terms of the GNU General Public License as published by
+# * the Free Software Foundation; either version 2 of the License, or
+# * (at your option) any later version.
+# *
+# * This program is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# * GNU General Public License for more details.
+# *
+# * You should have received a copy of the GNU General Public License
+# * along with this program; if not, write to the Free Software
+# * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+# * 02111-1307  USA
+# *
+# *  All comments concerning this program package may be sent to the
+# *  e-mail address 'scipion@cnb.csic.es'
+# *
+# **************************************************************************
+
+import pwem
+from .constants import *
+
+__version__ = "3.0.1"  # plugin version
+_logo = "icon.png"
+#_references = ['you2019']
+
+
+class Plugin(pwem.Plugin):
+    pass
+    @classmethod
+    def getEnviron(cls):
+        pass
+
+
+    def _defineVariables(cls):
+        pass
